1be11bc325a4eba0cc290175a5aa0f7c7cbe9a7f
JISU LEE <jsnote2013@gmail.com>
2020-06-04 20:17:45 +0900

    onResume까지 테스트

--- app/src/main/java/com/jisu/androidlifecycle_20200604/MainActivity.kt
+++ app/src/main/java/com/jisu/androidlifecycle_20200604/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.jisu.androidlifecycle_20200604
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Main화면", "onCreate 실행됨")
+
+        goToOtherBtn.setOnClickListener {
+            val intent = Intent(this, OtherActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("Main화면", "onResume 실행됨")
     }
 }
---
+++ app/src/main/java/com/jisu/androidlifecycle_20200604/OtherActivity.kt
@@ -0,0 +1,22 @@
+package com.jisu.androidlifecycle_20200604
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        Log.d("other화면", "onCreate 실행됨")
+
+        goBackBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
